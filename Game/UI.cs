21ab16e3f51f5331499d91d2990c2cbced72c634
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-13 15:32:50 +0100

    UI

---
+++ Game/UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class UI
+    {
+        internal static void Clear()
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+        }
+
+        internal static ConsoleKey GetKey() => Console.ReadKey(intercept: true).Key;
+        //{
+        //    return Console.ReadKey(intercept: true).Key;
+        //}
+    }
+}
