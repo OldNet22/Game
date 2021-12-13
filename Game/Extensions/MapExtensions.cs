6c31f99b98c49fa592cce1a8dcd30f7fac81ed90
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-13 15:32:59 +0100

    Extension

---
+++ Game/Extensions/MapExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Game.Entities;
+using Game.GameWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Extensions
+{
+    internal static class MapExtensions
+    {
+        [return: MaybeNull]
+        internal static IDrawable CreatureAtExtension(this List<Creature> creatures, Cell cell)
+        {
+            IDrawable? result = null;
+            foreach (Creature creature in creatures)
+            {
+                if (creature.Cell == cell)
+                {
+                    result = creature;
+                    break;
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
